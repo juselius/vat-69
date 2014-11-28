003ec62471bffc4da8935bcee2973536d1079ece
Jonas Juselius <jonas.juselius@uit.no>
2014-11-28 12:41:59 +0100

    Initial step towards drunkenness.

--- src/main.f90
+++ src/main.f90
@@ -1,6 +1,17 @@
 program vat69
+    use brew_m
+    use barrel_class
     implicit none
 
-    print *, "22 bottles of beer and vat 69."
+    integer :: nbottles
+    type(barrel_t) :: vat
+
+    nbottles = brew_beer(100.0, 0.5, 500.0)
+
+    call vat%init(119.24)
+    call vat%distil(2, 52.0)
+
+
+    print *, "22 bottles of beer and vat 69.", vat%vat_no
 end program
 
